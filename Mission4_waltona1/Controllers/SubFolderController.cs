75a6c8cd4f2bb1689b284e2804ed351134b99607
Angelina Walton <waltona1@byu.edu>
2023-02-06 19:39:26 -0700

    Added Mission 5

---
+++ Mission4_waltona1/Controllers/SubFolderController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Mission4_waltona1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission4_waltona1.Controllers
+{
+    public class SubFolderController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult calculation ()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult calculation (calculationModel model)
+        {
+            return View();
+        }
+    }
+}
---
+++ Mission4_waltona1/Models/calculationModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission4_waltona1.Models
+{
+    public class calculationModel
+    {
+        [Range(0, 100, ErrorMessage = "Input must be between 0 and 100")]
+        public decimal assignment { get; set; }
+        [Range(0, 100, ErrorMessage = "Input must be between 0 and 100")]
+        public decimal groupProject { get; set; }
+        [Range(0, 100, ErrorMessage = "Input must be between 0 and 100")]
+        public decimal quiz { get; set; }
+        [Range(0, 100, ErrorMessage = "Input must be between 0 and 100")]
+        public decimal midterm { get; set; }
+        [Range(0, 100, ErrorMessage = "Input must be between 0 and 100")]
+        public decimal final { get; set; }
+        [Range(0, 100, ErrorMessage = "Input must be between 0 and 100")]
+        public decimal intex { get; set; }
+
+    }
+}
